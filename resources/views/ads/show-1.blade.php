a6f6e3e12ee5eaf929ff5f0f5f3e4b989d2ae8da
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-10-26 00:34:28 -0600

    se agrego disable al form de comentario de anuncio

--- resources/views/ads/show-1.blade.php
+++ resources/views/ads/show-1.blade.php
@@ -61,12 +61,12 @@
 
                         <div class="form-group">
                             {!! Form::label('comment','Deja tu comentario:') !!}
-                            {!! Form::textarea('comment',null,['class'=>'form-control', 'id' => 'comment'/*, 'disabled'=>'disabled'*/]) !!}
+                            {!! Form::textarea('comment',null,['class'=>'form-control', 'id' => 'comment', 'disabled'=>'disabled']) !!}
                             {!! errors_for('comment',$errors) !!}
                         </div>
                         <div class="form-group">
 
-                            {!! Form::submit('Enviar',['class'=>'btn btn-primary'/*, 'disabled'=>'disabled'*/])!!}
+                            {!! Form::submit('Enviar',['class'=>'btn btn-primary', 'disabled'=>'disabled'])!!}
                             {!! link_to_route('ads.index','Regresar')!!}
                         </div>
 
@@ -82,12 +82,12 @@
 
                         <div class="form-group">
                             {!! Form::label('comment','Deja tu comentario:') !!}
-                            {!! Form::textarea('comment',null,['class'=>'form-control', 'id' => 'comment'/*, 'disabled'=>'disabled'*/]) !!}
+                            {!! Form::textarea('comment',null,['class'=>'form-control', 'id' => 'comment', 'disabled'=>'disabled']) !!}
                             {!! errors_for('comment',$errors) !!}
                         </div>
                         <div class="form-group">
 
-                            {!! Form::submit('Enviar',['class'=>'btn btn-primary'/*, 'disabled'=>'disabled'*/])!!}
+                            {!! Form::submit('Enviar',['class'=>'btn btn-primary', 'disabled'=>'disabled'])!!}
                             {!! link_to_route('ads.index','Regresar')!!}
                         </div>
 
@@ -102,12 +102,12 @@
 
                             <div class="form-group">
                                 {!! Form::label('comment','Deja tu comentario:') !!}
-                                {!! Form::textarea('comment',null,['class'=>'form-control', 'id' => 'comment'/*, 'disabled'=>'disabled'*/]) !!}
+                                {!! Form::textarea('comment',null,['class'=>'form-control', 'id' => 'comment', 'disabled'=>'disabled']) !!}
                                 {!! errors_for('comment',$errors) !!}
                             </div>
                             <div class="form-group">
 
-                                {!! Form::submit('Enviar',['class'=>'btn btn-primary'/*, 'disabled'=>'disabled'*/])!!}
+                                {!! Form::submit('Enviar',['class'=>'btn btn-primary', 'disabled'=>'disabled'])!!}
                                 {!! link_to_route('ads.index','Regresar')!!}
                             </div>
 
